100ccf1f5f1b41f0697badd57a0a204aad696c7c
MJHidan <mathiashidan@outlook.com>
2020-06-26 23:50:46 +0200

    Updates

---
+++ precache-manifest.da8fa40691dc1a9b2ebcf2c1a7a8964c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07752eb9f861f9a66bc26dc7ce58662d",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "26e4a9a52ed8b096104a",
+    "url": "/Robofriends/static/css/main.58ce14d3.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e4a9a52ed8b096104a",
+    "url": "/Robofriends/static/js/main.24f17c4c.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  },
+  {
+    "revision": "58067926630a060793246c021127df72",
+    "url": "/Robofriends/static/media/Warcraft-Yj2j.58067926.woff"
+  }
+]);
